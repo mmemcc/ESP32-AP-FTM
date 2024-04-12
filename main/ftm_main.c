a64ae81305a80f79597a53dc7f218f013ce00041
minji <mjk980917@gmail.com>
2024-04-12 14:31:00 +0900

    bw 변경

--- main/ftm_main.c
+++ main/ftm_main.c
@@ -17,9 +17,9 @@
 const char* SSID = "ESP32_AP_40";
 const char* PWD = "34081948";
 uint8_t channel = 1;
-uint8_t bw = 40; // 20 or 40
+uint8_t bw = 20; // 20 or 40
 int8_t power = 84;
-int16_t offset_cm = 100; // cm
+int16_t offset_cm = 0; // cm
 
 wifi_config_t g_ap_config = {
     .ap.max_connection = 4,
@@ -74,9 +74,7 @@ static void wifi_ap()
         second_chan = WIFI_SECOND_CHAN_NONE;
     }
 
-    esp_wifi_ftm_resp_set_offset(offset_cm);
-
-    
+    ESP_ERROR_CHECK(esp_wifi_ftm_resp_set_offset(offset_cm));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &g_ap_config));
 
     if (bw == 40) {
